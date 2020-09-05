c6e84022e720931741fca89d8335621b6746070b
jxmot <webdev@stupiddog.net>
2020-09-04 23:04:43 -0500

    httpget.js initial commit

---
+++ www/assets/js/httpget.js
@@ -0,0 +1,27 @@
+/*
+    Send a GET request and invoke a 
+    callback function when completed.
+*/
+function httpGet(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            var _resp = {};
+            var resp = this.responseText;
+            resp = resp.replace(/[\n\r]/g, '');
+            consolelog('httpGet - ' + resp);
+            if(resp.match(/^\{/g)) 
+                _resp = JSON.parse(resp);
+            else {
+                _resp.error = true;
+                // https://httpstatuses.com/
+                _resp.ret   = 500;
+                _resp.msg   = resp;
+            }
+            callback(_resp);
+        }
+    };
+    xmlhttp.open('GET', `${url}`, true);
+    xmlhttp.send();
+};
+
