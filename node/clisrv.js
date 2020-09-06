598f55b613eed84b443d5ed426a0389df7285f8a
jxmot <webdev@stupiddog.net>
2020-09-05 22:09:40 -0500

    added timed on/off control, added configurable console output

--- node/clisrv.js
+++ node/clisrv.js
@@ -4,19 +4,23 @@ module.exports = (function()  {
     const fs = require('fs');
     const url = require('url');
     const qstr = require('querystring');
+
+    const ccfg = require('./clisrvcfg.js');
     const mini = require('./minidiy.js');
-    const mcfg = require('./clisrvcfg.js');
+    mini.debug - ccfg.debug;
 
     clisrv = {
     };
 
     let server = {};
+    let timerid = {};
+    let state = 'n/a';
 
     clisrv.start = function() {
         server = http.createServer(handleRequest);
         // Starts the server.
-        server.listen(mcfg.port, function() {
-            console.log("Server is listening on PORT: " + mcfg.port);
+        server.listen(ccfg.port, function() {
+            consolelog("Server is listening on PORT: " + ccfg.port);
         });
     };
 
@@ -35,20 +39,42 @@ module.exports = (function()  {
         // call on different functions.
         switch (urlParts.pathname) {
             case '/info':
-                console.log('got info');
+                consolelog('got info');
                 mini.sendCmd('info', null, reply => {
                     res.writeHead(200, headers);
                     res.end(reply);
+                    state = JSON.parse(reply).data.switch;
+                    if((state === 'off') && 
+                       ((timerid._idleTimeout === undefined) || (timerid._idleTimeout === -1)) && 
+                       (ccfg.maxtime > 0)) {
+                        consolelog('STATE timer started - '+state);
+                        timerid = setTimeout(() => {
+                            mini.sendCmd('switch', ccfg.nextstate, dummy => {
+                                consolelog('ret to STATE = '+ccfg.nextstate);
+                            });
+                        },ccfg.maxtime);
+                    }
                 });
                 break;
 
             case '/switch':
-                console.log('got switch');
-                if(urlQuery.state === 'off') {
+                consolelog('got switch');
+                state = urlQuery.state;
+                if((urlQuery.state === ccfg.timedstate) && (ccfg.maxtime > 0)) {
                     // Start a timer for a configurable
                     // duration. When it expires turn the
                     // Mini back to ON.
-                    console.log('timed OFF');
+                    consolelog('timed STATE = '+ccfg.timedstate);
+                    timerid = setTimeout(() => {
+                        mini.sendCmd('switch', ccfg.nextstate, dummy => {
+                            consolelog('ret to STATE = '+ccfg.nextstate);
+                        });
+                    }, ccfg.maxtime);
+                }
+                if((timerid !== {}) && (urlQuery.state === ccfg.nextstate)) {
+                    clearTimeout(timerid);
+                    timerid = undefined;
+                    consolelog('timed STATE cleared');
                 }
                 mini.sendCmd('switch', urlQuery.state, reply => {
                     res.writeHead(200, headers);
@@ -58,5 +84,12 @@ module.exports = (function()  {
 
         };
     };
+
+    function consolelog(text) {
+        if(ccfg.debug) {
+            console.log(text);
+        }
+    };
+
     return clisrv;
 })();
