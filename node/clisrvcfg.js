61b5439de1c6df360a5b52f5fad8bb42c920211e
jxmot <webdev@stupiddog.net>
2020-09-05 22:06:52 -0500

    added items for configuring timed on/off

--- node/clisrvcfg.js
+++ node/clisrvcfg.js
@@ -1,4 +1,15 @@
 module.exports = {
+    // use this port with the client -
+    //      http://127.0.0.1:6464/.....
     // "mini" on a phone pad
-    port: '6464'
+    port: '6464',
+    // the state with a timeout
+    timedstate: 'off',
+    nextstate: 'on',
+    // set to 0 to disable timeout
+    //      10800000 = 3 hours
+    //       3600000 = 1 hour
+    maxtime: 10800000,
+    // enable/disable console output
+    debug: true
 };
