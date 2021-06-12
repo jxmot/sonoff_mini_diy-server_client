985c29f72036bbb3df78eb3f87772177a19759ab
jxmot <webdev@stupiddog.net>
2021-06-12 13:07:27 -0500

    added secHMS() to convert seconds to "HH:MM:SS" string

--- node/clisrv.js
+++ node/clisrv.js
@@ -31,6 +31,20 @@ module.exports = (function(_log)  {
         });
     };
 
+    function secHMS(sec) {
+        let sec_num = (typeof sec === 'string' ? sec_num = parseInt(sec) : sec);
+
+        let hours   = Math.floor(sec_num / 3600);
+        let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        let seconds = sec_num - (hours * 3600) - (minutes * 60);
+    
+        if (hours   < 10) {hours   = "0"+hours;}
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+
+        return hours+':'+minutes+':'+seconds;
+    }
+
     function handleRequest(req, res) {
 
         const headers = {
