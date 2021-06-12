aee63d4a7426d24c3aa3a83ac9f4b6e2333167bf
jxmot <webdev@stupiddog.net>
2021-06-12 11:00:12 -0500

    renamed index.js to mini-server.js

--- node/index.js
+++
@@ -1,7 +0,0 @@
-/*
-    https://github.com/jxmot/sonoff_mini_diy-server_client
-
-    This is the API server side in the Sonoff Mini project.
-*/
-const clisrv = require('./clisrv.js');
-clisrv.start();
---
+++ node/minidiy-server.js
@@ -0,0 +1,30 @@
+'use strict';
+/*
+    https://github.com/jxmot/sonoff_mini_diy-server_client
+
+    This is the API server side in the Sonoff Mini project.
+*/
+// Run-Time Logging
+const Log = require('simple-text-log');
+const logOut = new Log(require('./runlogopt.js'));
+
+var logoutin = true;
+// pass this function around to the other modules
+function _log(payload) {
+    if(logoutin === true) logOut.writeTS(payload);
+    else console.log(payload);
+};
+
+const scriptName = require('path').basename(__filename);
+function log(payload) {
+    _log(`${scriptName} - ${payload}`);
+};
+
+log('*******************************************');
+log('start');
+
+/*
+    Initialize and start the server...
+*/
+const clisrv = require('./clisrv.js')(_log);
+clisrv.start();
